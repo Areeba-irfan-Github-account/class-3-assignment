2167091ff73d3c45e01f071b0dbe7c2f87efaed9
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-09-23 19:14:16 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen bg-black text-white font-bold text-5xl flex justify-center items-center">
+        It's Areeba Irfan
+      </div>
+    </>
+  );
+}
